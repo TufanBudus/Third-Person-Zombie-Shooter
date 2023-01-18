07f3367596609dfde55d298dbfeb7dfca8fc8577
ik <bruh@lmbao.lol>
2023-01-18 14:06:17 +0100

    ben ja min

---
+++ Project/Source/ThirdPersonZombie/Public/Zombie.h
@@ -0,0 +1,29 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Zombie.generated.h"
+
+UCLASS()
+class THIRDPERSONZOMBIE_API AZombie : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AZombie();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Project/Source/ThirdPersonZombie/ThirdPersonZombie.cpp
@@ -0,0 +1,6 @@
+
+
+#include "ThirdPersonZombie.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPersonZombie, "ThirdPersonZombie" );
